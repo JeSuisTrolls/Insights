ab8092a000b6eb3efc7f1c97d5e8b80996af7b06
JeSuisTrolls <105175788+JeSuisTrolls@users.noreply.github.com>
2026-04-23 16:13:56 -0400

    Refactor entity removal handling in EntityListener
    Refactor entity removal handling to support player-specific logic and improve clarity.

--- Insights-Core/src/main/java/dev/frankheijden/insights/listeners/EntityListener.java
+++ Insights-Core/src/main/java/dev/frankheijden/insights/listeners/EntityListener.java
@@ -168,16 +168,30 @@ public class EntityListener extends InsightsListener {
         handleEntityRemoveFromWorld(event.getEntity());
     }
 
-    protected void handleEntityRemoveFromWorld(Entity entity) {
-        if (!entity.isDead()) return;
-        if (removedEntities.remove(entity.getUniqueId())) return;
-
-        if (isSpawnLimited(entity.getType())) {
-            handleModification(entity.getLocation(), entity.getType(), -1);
+    protected void handleEntityRemoval(Entity entity, boolean isPlayer) {
+        EntityType entityType = entity.getType();
+    
+        if (isSpawnLimited(entityType)) {
+            removedEntities.add(entity.getUniqueId());
+            handleModification(entity.getLocation(), entityType, -1);
             return;
         }
-
-        handleEntityRemoval(entity, false);
+    
+        if (!LIMITED_ENTITIES.contains(entityType)) return;
+    
+        Location location = entity.getLocation();
+        int delta = 1;
+    
+        if (isPlayer) {
+            removedEntities.add(entity.getUniqueId());
+            Optional<Player> player = getPlayerKiller(entity);
+            if (player.isPresent()) {
+                handleRemoval(player.get(), location, ScanObject.of(entityType), delta);
+                return;
+            }
+        }
+    
+        handleModification(location, entityType, -delta);
     }
 
     protected boolean handleEntityPlace(Player player, Entity entity) {
